41288f8f7561ed0fd78a991205e48eba93185555
benjaminsierr <benjaminsierr@gmail.com>
2022-01-30 12:09:41 -0700

    milestone 4: Unit tests and jacoco coverage report

--- src/main/java/edu/byu/is560r/bsierra1notesapinew/controller/UserController.java
+++ src/main/java/edu/byu/is560r/bsierra1notesapinew/controller/UserController.java
@@ -1,20 +1,22 @@
 package edu.byu.is560r.bsierra1notesapinew.controller;
 
-import edu.byu.is560r.bsierra1notesapinew.service.DummyUserService;
+//import edu.byu.is560r.bsierra1notesapinew.service.DummyUserService;
 import edu.byu.is560r.bsierra1notesapinew.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-// test committ
+
 @RestController
+@RequestMapping("/v1")
 @AllArgsConstructor // This means we don't need a constructor
 public class UserController {
 
-    UserService userService; // This is the injected class
+    private UserService userService; // This is the injected class
+
     @GetMapping("/user") // REST Endpoint
-    public String getUser(@RequestParam String username) {
-        var user = userService.getUsername(username);
-        return user;
+    public String getUserName(@RequestParam(name = "user_id") String userName) {
+        return userService.getUserName(userName);
     }
 }
